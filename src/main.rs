bc174556ff1149162ea7f81f0ca9027c0e1646c0
José Carlos <josecarlosskar@gmail.com>
2020-09-09 19:26:33 -0300

    import dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,13 @@
+use rustyline::error::ReadlineError;
+use rustyline::Editor;
+mod gl_env;
+mod gl_exception;
+mod gl_lexer;
+mod gl_runtime;
+mod gl_token;
+mod gl_token_position;
+mod gl_tokens;
+
+const GL_VERSION: &str = "0.1.0-alpha";
+
 fn main() {}
