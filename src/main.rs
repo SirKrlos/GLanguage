055349c7377e0134ff534b7bc180758322ba234a
José Carlos <josecarlosskar@gmail.com>
2020-09-09 19:27:35 -0300

    create function: (show_version, show_help)

--- src/main.rs
+++ src/main.rs
@@ -11,3 +11,21 @@ mod gl_tokens;
 const GL_VERSION: &str = "0.1.0-alpha";
 
 fn main() {}
+
+fn show_version() {
+	println!("GLanguage v{}", GL_VERSION);
+}
+
+fn show_help() {
+	show_version();
+	println!("");
+	println!("Usage: gl [options] [arguments]");
+	println!("       gl run main.gl [arguments]");
+	println!("");
+	println!("Options:");
+	println!("  --help        print this help message and exit");
+	println!("  --version     print the glanguage version number and exit");
+	println!("  cmd           run program passed as string");
+	println!("  run           run program from script file");
+	println!("");
+}
