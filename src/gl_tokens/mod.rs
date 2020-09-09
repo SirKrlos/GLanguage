8a78e5bc69ecec5d8611b5f52cf4469d3b519b33
José Carlos <josecarlosskar@gmail.com>
2020-09-08 21:19:30 -0300

    add: constants (SPACES, PUNCTUATIONS, DIGITS, LETTERS, LETTERS_DIGITS)

---
+++ src/gl_tokens/mod.rs
@@ -0,0 +1,5 @@
+pub const SPACES: [&str; 4] = ["\r", "\n", "\t", " "];
+pub const PUNCTUATIONS: [&str; 1] = [";"];
+pub const DIGITS: &str = "0123456789";
+pub const LETTERS: &str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_";
+pub const LETTERS_DIGITS: &str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_0123456789";
