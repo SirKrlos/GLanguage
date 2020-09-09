17065b36e7d9b713024f44ffb6b289436bcdc85a
José Carlos <josecarlosskar@gmail.com>
2020-09-08 21:23:15 -0300

    create component: Tokens

--- src/gl_tokens/mod.rs
+++ src/gl_tokens/mod.rs
@@ -3,3 +3,15 @@ pub const PUNCTUATIONS: [&str; 1] = [";"];
 pub const DIGITS: &str = "0123456789";
 pub const LETTERS: &str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_";
 pub const LETTERS_DIGITS: &str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_0123456789";
+
+pub enum Tokens {
+	EOF,
+}
+
+impl Tokens {
+	pub fn copy(&self) -> Tokens {
+		match &self {
+			Tokens::EOF => Tokens::EOF,
+		}
+	}
+}
