398f0b10b1a242d7f4e2dde1e99a3a05e3d0eb64
José Carlos <josecarlosskar@gmail.com>
2020-09-09 19:00:02 -0300

    create method 'run_codetext' for component: RunTime

--- src/gl_runtime/mod.rs
+++ src/gl_runtime/mod.rs
@@ -1,7 +1,17 @@
+use crate::gl_lexer::Lexer;
+
 pub struct RunTime {}
 
 impl RunTime {
 	pub fn new() -> RunTime {
 		RunTime {}
+	}
+
+	pub fn run_codetext(&mut self, filename: String, codetext: String) -> bool {
+		let mut lexer: Lexer = Lexer::new(String::from(filename), String::from(codetext));
+		if lexer.run() == true {
+			return true;
+		}
+		return false;
 	}
 }
