5893c13d19b25978573dfac6d24f2d0ef0caa7e4
José Carlos <josecarlosskar@gmail.com>
2020-09-10 16:32:23 -0300

    update component: Lexer

--- src/gl_lexer/mod.rs
+++ src/gl_lexer/mod.rs
@@ -7,17 +7,17 @@ pub struct Lexer {
 	codetext: String,
 	chars: Vec<char>,
 	linestext: Vec<String>,
-	position: TokenPosition,
+	pub position: TokenPosition,
 	current_char: String,
 	current_linetext: String,
 	pub tokens: Vec<Token>,
 }
 
 impl Lexer {
-	pub fn new(filename: String, codetext: String) -> Lexer {
+	pub fn new(filename: String, codetext: String, lineno: u32) -> Lexer {
 		let mut chars: Vec<char> = codetext.chars().collect::<Vec<char>>();
 		let mut linestext: Vec<String> = codetext.lines().map(|line| line.to_string()).collect::<Vec<String>>();
-		let position: TokenPosition = TokenPosition::new(0, 0, 0);
+		let position: TokenPosition = TokenPosition::new(0, lineno, 0);
 		let current_char: String = if chars.len() > 0 { chars.remove(0).to_string() } else { String::new() };
 		let current_linetext: String = if linestext.len() > 0 { linestext.remove(0) } else { String::from(&codetext) };
 		let tokens: Vec<Token> = Vec::new();
