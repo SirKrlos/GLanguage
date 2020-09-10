4005a14260669b4979985568b01967d9c4e9fbf7
José Carlos <josecarlosskar@gmail.com>
2020-09-10 16:36:47 -0300

    update method 'run_codetext' from component: RunTime

--- src/gl_runtime/mod.rs
+++ src/gl_runtime/mod.rs
@@ -7,16 +7,18 @@ pub struct RunTime {
 
 impl RunTime {
 	pub fn new(lineno: u32) -> RunTime {
-		RunTime { lineno: lineno }
+		RunTime { lineno }
 	}
 
 	pub fn run_codetext(&mut self, filename: String, codetext: String) -> bool {
-		let mut lexer: Lexer = Lexer::new(String::from(filename), String::from(codetext));
+		let mut lexer: Lexer = Lexer::new(String::from(filename), String::from(codetext), self.lineno);
 		if lexer.run() == true {
+			self.lineno = lexer.position.lineno;
 			return true;
 		}
+		self.lineno = lexer.position.lineno;
 
-		let mut parser: Parser = Parser::new(lexer.tokens);
+		let mut parser: Parser = Parser::new(lexer.copy_tokens());
 		if parser.run() == true {
 			return true;
 		}
