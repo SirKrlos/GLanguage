19bd4e34caae2b27b5d2e33d6550c5ce60cc315b
José Carlos <josecarlosskar@gmail.com>
2020-09-11 21:38:08 -0300

    update component: Statement

--- src/gl_statement/mod.rs
+++ src/gl_statement/mod.rs
@@ -1,2 +1,14 @@
+use crate::gl_token::Token;
+
 pub enum Statement {
+	Expression(Expression),
+}
+
+pub enum Expression {
+	Integer(ValueWithToken),
+}
+
+pub struct ValueWithToken {
+	pub value: String,
+	pub token: Token,
 }
