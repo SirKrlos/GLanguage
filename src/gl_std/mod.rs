c52621bcc95e63f80edb56465bf182c4e2a31b5d
José Carlos <josecarlosskar@gmail.com>
2020-09-16 20:33:45 -0300

    create component: Object

---
+++ src/gl_std/mod.rs
@@ -0,0 +1,28 @@
+mod gl_std_integer;
+pub use gl_std_integer::GLInteger;
+
+pub enum Object {
+	Null,
+	Integer(GLInteger),
+	String(String),
+}
+
+impl Object {
+	pub fn copy(&self) -> Object {
+		match &self {
+			Object::Null => Object::Null,
+			Object::Integer(integer) => Object::Integer(GLInteger::from_string(integer.to_string())),
+			Object::String(string) => Object::String(String::from(string)),
+		}
+	}
+}
+
+impl std::fmt::Display for Object {
+	fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+		match &self {
+			Object::Null => write!(f, "Null"),
+			Object::Integer(integer) => write!(f, "{}", integer.to_string()),
+			Object::String(string) => write!(f, "{}", &string.to_string()),
+		}
+	}
+}
