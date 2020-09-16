bbeafd7d4620e6f1d9fc0ff29969f94ecd81bacb
José Carlos <josecarlosskar@gmail.com>
2020-09-16 20:50:50 -0300

    update component: Env

--- src/gl_env/mod.rs
+++ src/gl_env/mod.rs
@@ -1,7 +1,24 @@
-pub struct Env {}
+use crate::gl_std::Object;
+use std::collections::HashMap;
+
+pub struct Env {
+	env: HashMap<String, Object>,
+}
 
 impl Env {
 	pub fn new() -> Env {
-		Env {}
+		Env { env: std::collections::HashMap::new() }
+	}
+
+	pub fn set(&mut self, key: &String, value: Object) {
+		self.env.insert(String::from(key), value);
+	}
+
+	pub fn get(&self, key: &String) -> Option<Object> {
+		self.env.get(key).map(|value| value.copy())
+	}
+
+	pub fn exist(&self, key: &String) -> bool {
+		self.env.contains_key(key)
 	}
 }
