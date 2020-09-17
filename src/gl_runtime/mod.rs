2994e7e66bc362b7a39c1a419b4c15d28e0e4fe4
José Carlos <josecarlosskar@gmail.com>
2020-09-16 21:02:32 -0300

    update component: RunTime

--- src/gl_runtime/mod.rs
+++ src/gl_runtime/mod.rs
@@ -1,13 +1,19 @@
+use crate::gl_env::Env;
+use crate::gl_eval::Eval;
 use crate::gl_lexer::Lexer;
 use crate::gl_parser::Parser;
+use crate::gl_std::{load_std_to_env, Object};
 
 pub struct RunTime {
 	lineno: u32,
+	env: Env,
 }
 
 impl RunTime {
 	pub fn new(lineno: u32) -> RunTime {
-		RunTime { lineno }
+		let mut env: Env = Env::new();
+		load_std_to_env(&mut env);
+		RunTime { lineno, env }
 	}
 
 	pub fn run_codetext(&mut self, filename: String, codetext: String) -> bool {
@@ -21,6 +27,15 @@ impl RunTime {
 		let mut parser: Parser = Parser::new(lexer.copy_tokens());
 		if parser.run() == true {
 			return true;
+		}
+
+		let eval: Eval = Eval::new();
+		let results: Vec<Object> = eval.eval_return_scope(parser.asts, &mut self.env);
+		for result in &results {
+			match result {
+				Object::Null => (),
+				_ => println!("{}", result),
+			}
 		}
 
 		return false;
