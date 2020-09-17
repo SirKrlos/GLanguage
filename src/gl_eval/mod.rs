2b1ab260c4107b6d1e520ec356bd67d61650ffa2
José Carlos <josecarlosskar@gmail.com>
2020-09-16 21:43:03 -0300

    create component: Eval

---
+++ src/gl_eval/mod.rs
@@ -0,0 +1,12 @@
+use crate::gl_env::Env;
+use crate::gl_statement::{Expression, Statement};
+use crate::gl_std::{GLInteger, Object};
+use crate::gl_exception::Exception;
+
+pub struct Eval {}
+
+impl Eval {
+	pub fn new() -> Eval {
+		Eval {}
+	}
+}
